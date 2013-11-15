bcebb4ffcc0990bc2c0a8de6f14247df1f8f343a
Andre Behrens <mrandre@gmail.com>
2013-11-15 16:17:06 -0500

    Re-spaced everything, fleshed out the task.

--- tasks/chinstrap.js
+++ tasks/chinstrap.js
@@ -9,43 +9,84 @@
 'use strict';
 
 module.exports = function(grunt) {
+	var path = require('path');	
+	var chinstrap = require("chinstrap-engine");
+	var VERSION = require(path.join('..','node_modules','chinstrap-engine','package.json')).version;
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-  var chinstrap = require("chinstrap")
-  grunt.registerMultiTask('chinstrap', 'A grunt plugin to compile chinstrap templates', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+	grunt.log.writeln('Rendering with Chinstrap version ' + VERSION);
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        var template = grunt.file.read(filepath);
-		console.log("Template content.", template);
-      }).join(grunt.util.normalizelf(options.separator));
+	grunt.registerMultiTask('chinstrap', 'Compile chinstrap templates into built config files', function() {
+		var bannerText, options, now;
 
-      // Handle options.
-      src += options.punctuation;
+		now = new Date().toString();
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+		bannerText = '/* Compiled with Chinstrap ' +
+			VERSION + ' ' + now + ' */\n\n';
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+		options = this.options({
+			wrap: null,
+			skipRender: false,
+			banner: bannerText
+		});
 
+		this.files.forEach(generateOutputFile);
+
+		function processFiles(f) {
+			return f.src
+				.filter(validFile)
+				.map(compileTemplateToFunction)
+				.join(',');
+		}
+
+		function validFile(filepath) {
+			if (!grunt.file.exists(filepath)) {
+				grunt.log.warn('Source file "' + filepath + '" not found.');
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		function compileTemplateToFunction(filepath) {
+			var compiled, template, templateName, fn;
+			// Read file source.
+			grunt.verbose.writeln("Template: " + filepath)
+			templateName = extractName(filepath);
+			grunt.verbose.writeln("Template name: " + templateName);
+			template = grunt.file.read(filepath);
+			grunt.verbose.writeln("Template content: " + template);
+			if (options.skipRender) {
+				compiled = template;
+			} else {
+				fn = new Function("config", chinstrap.render(template));
+				compiled = fn.toString();
+			}
+			grunt.verbose.writeln("Compiled " + compiled);
+			return "'" + templateName + "':" + compiled;
+		}
+
+		function generateOutputFile(f) {
+			var strResult = processFiles(f);
+			grunt.verbose.writeln("Source: " + strResult);
+			strResult = "{" + strResult + "}";
+
+			if (options.wrap) {
+				strResult = options.wrap.replace("***", strResult);
+			}
+
+			if (options.banner) {
+				strResult = options.banner + strResult;
+			}
+			// Write the destination file.
+			grunt.file.write(f.dest, strResult);
+			grunt.log.writeln('File "' + f.dest + '" created.');
+		}
+
+		function extractName(filePath) {
+			var parts = filePath.split("/");
+			var fileBase = parts[parts.length - 1];
+			var name = fileBase.split(".")[0];
+			return name;
+		}
+	});
 };
